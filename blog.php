1521698bc8f2cc8de01523fb86b24683e71883d8
Fabricio Soares <eu@fabriciosoares.com>
2010-06-07 16:53:56 -0300

    Blog file

---
+++ blog.php
@@ -0,0 +1,113 @@
+<?php
+	/* Template Name: Blog */
+
+	$BODY_CLASS = 'blog';
+
+	get_header();
+?>
+		<div id="content">
+			<div id="preview">
+				<div class="preview-post select">
+					<h3>Lorem Ipsum dolor color</h3>
+					<p class="date">01/05/2010</p>
+					<p>Integer bibendum mi id velit lobortis id venenatis elit hendrerit. Pellentesque ut sapien ipsum. Praesent eu vulputate nibh</p>
+				</div>
+				<div class="preview-post">
+					<h3><a href="#" title="Lorem Ipsum dolor color">Lorem Ipsum dolor color</a></h3>
+					<p class="date">01/05/2010</p>
+					<p>Integer bibendum mi id velit lobortis id venenatis elit hendrerit. Pellentesque ut sapien ipsum. Praesent eu vulputate nibh</p>
+				</div>
+				<div class="preview-post">
+					<h3><a href="#" title="Lorem Ipsum dolor color">Lorem Ipsum dolor color</a></h3>
+					<p class="date">01/05/2010</p>
+					<p>Integer bibendum mi id velit lobortis id venenatis elit hendrerit. Pellentesque ut sapien ipsum. Praesent eu vulputate nibh</p>
+				</div>
+				<div class="preview-post">
+					<h3><a href="#" title="Lorem Ipsum dolor color">Lorem Ipsum dolor color</a></h3>
+					<p class="date">01/05/2010</p>
+					<p>Integer bibendum mi id velit lobortis id venenatis elit hendrerit. Pellentesque ut sapien ipsum. Praesent eu vulputate nibh</p>
+				</div>
+				<div class="preview-post">
+					<h3><a href="#" title="Lorem Ipsum dolor color">Lorem Ipsum dolor color</a></h3>
+					<p class="date">01/05/2010</p>
+					<p>Integer bibendum mi id velit lobortis id venenatis elit hendrerit. Pellentesque ut sapien ipsum. Praesent eu vulputate nibh</p>
+				</div>
+			</div><!--fim #preview-->
+			<div class="post">
+				<span class="title">Blog</span>
+				<h2>Lorem Ipsum dolor color</h2>
+					<p class="date">01/05/2010</p>
+					<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed nulla tortor, iaculis eu dignissim pellentesque, malesuada vel est. In tortor dolor, consectetur consequat blandit sit amet, condimentum non odio. In adipiscing urna sit amet odio volutpat id tincidunt eros sodales. Pellentesque non velit libero, id eleifend nisl. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; In in semper libero.</p>
+					<p>Integer bibendum mi id velit lobortis id venenatis elit hendrerit. Pellentesque ut sapien ipsum. Praesent eu vulputate nibh. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Etiam eu diam sed risus cursus mattis. Pellentesque cursus nibh eu nisi laoreet ultricies. Integer cursus dignissim risus et varius. Nunc lorem odio, volutpat ut commodo non, posuere in enim. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Aliquam malesuada ante id orci sodales adipiscing. Nunc vitae risus vitae turpis eleifend vestibulum ut vel ligula. Nulla eget commodo ante. Donec a pretium arcu.</p>
+					<p>Vestibulum malesuada neque a felis tincidunt facilisis elementum ipsum tempor. Aenean eget elit dui, eget dictum turpis. Nam dignissim tempor ornare. Phasellus sodales magna sed quam porta vitae congue urna viverra. Suspendisse vitae lorem elit, at sollicitudin lectus. Etiam venenatis malesuada aliquam. Nunc nec nunc quis tellus pellentesque malesuada placerat eu est. Etiam sed dui vel diam porta porttitor. Etiam at orci eget dui iaculis condimentum. In convallis tincidunt augue at dapibus.</p>
+					<p>Morbi tristique mattis odio non feugiat. Fusce ultricies justo lectus. Ut ac tristique neque. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque tincidunt ante vel magna fringilla eu consectetur arcu pulvinar. Nulla id erat sem. Cras bibendum pellentesque nunc, eget rutrum arcu viverra vitae. Donec egestas pulvinar mi at porttitor. Nullam lectus ante, elementum a iaculis nec, rhoncus non eros. Quisque consequat congue mauris vel porta.</p>
+					<p>Sed eleifend faucibus ligula, et vestibulum urna tempus non. Maecenas ac eros turpis, ac tincidunt neque. Sed a nunc ut nibh aliquet dapibus. Etiam egestas eros ac eros egestas eu interdum odio aliquet. Phasellus eu eleifend mi. Donec mi tortor, rhoncus at commodo eget, tincidunt et massa. Sed molestie neque a lorem tempor semper. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Sed egestas bibendum ipsum id dignissim. Praesent tortor odio, rhoncus et tristique sed, sollicitudin in metus. Morbi scelerisque tincidunt lorem nec vestibulum. Donec dolor tortor, pretium non congue non, tempus ut tortor. Mauris nec est magna, quis egestas tellus.</p>
+				<div id="comments">
+					<div class="comment">
+						<div class="infoUser">
+							<img src="images/avatar.png" alt="Imagem de visualização do usuário" />
+							<span class="date">27/10/2009</span>
+							<span class="user" >Severino Jose do Santos de Oliveira e Silva</span>
+						</div>
+						<p>Nunc porttitor tincidunt magna, at rutrum lorem molestie eu. Proin vitae magna elit. Etiam enim dui, vestibulum eu vestibulum eget, egestas nec justo. Aenean at mi arcu, nec egestas velit. Aenean vitae justo augue.</p>
+					</div><!--fim .comment-->
+					<div class="comment">
+						<div class="infoUser">
+							<img src="images/avatar.png" alt="Imagem de visualização do usuário" />
+							<span class="date">27/10/2009</span>
+							<span class="user" >Severino Jose do Santos de Oliveira e Silva</span>
+						</div>
+						<p>Nunc porttitor tincidunt magna, at rutrum lorem molestie eu. Proin vitae magna elit. Etiam enim dui, vestibulum eu vestibulum eget, egestas nec justo. Aenean at mi arcu, nec egestas velit. Aenean vitae justo augue.</p>
+					</div><!--fim .comment-->
+					<div class="comment">
+						<div class="infoUser">
+							<img src="images/avatar.png" alt="Imagem de visualização do usuário" />
+							<span class="date">27/10/2009</span>
+							<span class="user" >Severino Jose do Santos de Oliveira e Silva</span>
+						</div>
+						<p>Nunc porttitor tincidunt magna, at rutrum lorem molestie eu. Proin vitae magna elit. Etiam enim dui, vestibulum eu vestibulum eget, egestas nec justo. Aenean at mi arcu, nec egestas velit. Aenean vitae justo augue.</p>
+							<div class="comment">
+							<div class="infoUser">
+								<img src="images/avatar.png" alt="Imagem de visualização do usuário" />
+								<span class="date">27/10/2009</span>
+								<span class="user" >Severino Jose do Santos de Oliveira e Silva</span>						
+							</div>
+							<p>Nunc porttitor tincidunt magna, at rutrum lorem molestie eu. Proin vitae magna elit. Etiam enim dui, vestibulum eu vestibulum eget, egestas nec justo. Aenean at mi arcu, nec egestas velit. Aenean vitae justo augue.</p>
+								<div class="comment">
+									<div class="infoUser">
+										<img src="images/avatar.png" alt="Imagem de visualização do usuário" />
+										<span class="date">27/10/2009</span>
+										<span class="user" >Severino Jose do Santos de Oliveira e Silva</span>
+									</div>
+									<p>Nunc porttitor tincidunt magna, at rutrum lorem molestie eu. Proin vitae magna elit. Etiam enim dui, vestibulum eu vestibulum eget, egestas nec justo. Aenean at mi arcu, nec egestas velit. Aenean vitae justo augue.</p>
+							</div><!--fim .comment-->
+						</div><!--fim .comment-->
+					</div><!--fim .comment-->
+					<div class="comment">
+						<div class="infoUser">
+							<img src="images/avatar.png" alt="Imagem de visualização do usuário" />
+							<span class="date">27/10/2009</span>
+							<span class="user" >Severino Jose do Santos de Oliveira e Silva</span>
+						</div>
+						<p>Nunc porttitor tincidunt magna, at rutrum lorem molestie eu. Proin vitae magna elit. Etiam enim dui, vestibulum eu vestibulum eget, egestas nec justo. Aenean at mi arcu, nec egestas velit. Aenean vitae justo augue.</p>
+					</div><!--fim .comment-->
+					<div class="comment">
+						<div class="infoUser">
+							<img src="images/avatar.png" alt="Imagem de visualização do usuário" />
+							<span class="date">27/10/2009</span>
+							<span class="user" >Severino Jose do Santos de Oliveira e Silva</span>
+						</div>
+						<p>Nunc porttitor tincidunt magna, at rutrum lorem molestie eu. Proin vitae magna elit. Etiam enim dui, vestibulum eu vestibulum eget, egestas nec justo. Aenean at mi arcu, nec egestas velit. Aenean vitae justo augue.</p>
+					</div><!--fim .comment-->
+				</div><!--fim #comments-->
+			</div><!--fim .post-->
+			<div id="widgets">
+				<div id="social1">
+					<p>Lorem ipsum</p>
+				</div>
+				<div id="social2">
+					<p>Lorem ipsum</p>
+				</div>
+			</div><!--fim #widgets-->
+		</div><!--fim #content-->
+<?php get_footer(); ?>
