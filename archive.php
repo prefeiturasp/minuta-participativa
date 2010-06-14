ed5b885e535eff54144ce182c688d2808825121f
Lincoln de Sousa <lincoln@minaslivre.org>
2010-06-14 10:53:14 -0300

    Setting up a layout for the archive.php page

--- archive.php
+++ archive.php
@@ -7,82 +7,85 @@
 get_header();
 ?>
 
-	<div id="content" class="narrowcolumn post" role="main">
+<div id="content" class="narrowcolumn" role="main">
 
-		<?php if (have_posts()) : ?>
+  <div class="post">
 
- 	  <?php $post = $posts[0]; // Hack. Set $post so that the_date() works. ?>
- 	  <?php /* If this is a category archive */ if (is_category()) { ?>
-		<h2 class="pagetitle">Lista de artigos para &#8216;<?php single_cat_title(); ?>&#8217;</h2>
- 	  <?php /* If this is a tag archive */ } elseif( is_tag() ) { ?>
-		<h2 class="pagetitle">Posts Tagged &#8216;<?php single_tag_title(); ?>&#8217;</h2>
- 	  <?php /* If this is a daily archive */ } elseif (is_day()) { ?>
-		<h2 class="pagetitle">Archive for <?php the_time('F jS, Y'); ?></h2>
- 	  <?php /* If this is a monthly archive */ } elseif (is_month()) { ?>
-		<h2 class="pagetitle">Archive for <?php the_time('F, Y'); ?></h2>
- 	  <?php /* If this is a yearly archive */ } elseif (is_year()) { ?>
-		<h2 class="pagetitle">Archive for <?php the_time('Y'); ?></h2>
-	  <?php /* If this is an author archive */ } elseif (is_author()) { ?>
-		<h2 class="pagetitle">Arquivo do autor</h2>
- 	  <?php /* If this is a paged archive */ } elseif (isset($_GET['paged']) && !empty($_GET['paged'])) { ?>
-		<h2 class="pagetitle">Blog Archives</h2>
- 	  <?php } ?>
+    <?php if (have_posts()) : ?>
+
+    <?php $post = $posts[0]; // Hack. Set $post so that the_date() works. ?>
+    <?php /* If this is a category archive */ if (is_category()) { ?>
+    <h2 class="pagetitle">Lista de artigos para &#8216;<?php single_cat_title(); ?>&#8217;</h2>
+    <?php /* If this is a tag archive */ } elseif( is_tag() ) { ?>
+    <h2 class="pagetitle">Posts Tagged &#8216;<?php single_tag_title(); ?>&#8217;</h2>
+    <?php /* If this is a daily archive */ } elseif (is_day()) { ?>
+    <h2 class="pagetitle">Archive for <?php the_time('F jS, Y'); ?></h2>
+    <?php /* If this is a monthly archive */ } elseif (is_month()) { ?>
+    <h2 class="pagetitle">Archive for <?php the_time('F, Y'); ?></h2>
+    <?php /* If this is a yearly archive */ } elseif (is_year()) { ?>
+    <h2 class="pagetitle">Archive for <?php the_time('Y'); ?></h2>
+    <?php /* If this is an author archive */ } elseif (is_author()) { ?>
+    <h2 class="pagetitle">Arquivo do autor</h2>
+    <?php /* If this is a paged archive */ } elseif (isset($_GET['paged']) && !empty($_GET['paged'])) { ?>
+    <h2 class="pagetitle">Blog Archives</h2>
+    <?php } ?>
 
 
-		<div class="navigation">
-			<div class="alignleft"><?php next_posts_link('&laquo; Older Entries') ?></div>
-			<div class="alignright"><?php previous_posts_link('Newer Entries &raquo;') ?></div>
-		</div>
+    <div class="navigation">
+      <div class="alignleft"><?php next_posts_link('&laquo; Older Entries') ?></div>
+      <div class="alignright"><?php previous_posts_link('Newer Entries &raquo;') ?></div>
+    </div>
 
-		<?php while (have_posts()) : the_post(); ?>
-		<div <?php post_class() ?>>
-				<h3 id="post-<?php the_ID(); ?>"><a href="<?php the_permalink() ?>" rel="bookmark" title="Permanent Link to <?php the_title_attribute(); ?>"><?php the_title(); ?></a></h3>
-				<small><?php the_time('l, F jS, Y') ?></small>
+    <?php while (have_posts()) : the_post(); ?>
+    <div>
+      <h3 id="post-<?php the_ID(); ?>"><a href="<?php the_permalink() ?>" rel="bookmark" title="Permanent Link to <?php the_title_attribute(); ?>"><?php the_title(); ?></a></h3>
+      <small><?php the_time('l, F jS, Y') ?></small>
 
-				<div class="entry">
-					<?php the_content() ?>
-				</div>
+      <div class="entry">
+        <?php the_content() ?>
+      </div>
 
-				<p class="postmetadata"><?php the_tags('Tags: ', ', ', '<br />'); ?> Posted in <?php the_category(', ') ?> | <?php edit_post_link('Edit', '', ' | '); ?>  <?php comments_popup_link('No Comments &#187;', '1 Comment &#187;', '% Comments &#187;'); ?></p>
+      <p class="postmetadata"><?php the_tags('Tags: ', ', ', '<br />'); ?> Posted in <?php the_category(', ') ?> | <?php edit_post_link('Edit', '', ' | '); ?>  <?php comments_popup_link('No Comments &#187;', '1 Comment &#187;', '% Comments &#187;'); ?></p>
 
-			</div>
+    </div>
 
-		<?php endwhile; ?>
+    <?php endwhile; ?>
 
-		<div class="navigation">
-			<div class="alignleft"><?php next_posts_link('&laquo; Anteriores') ?></div>
-			<div class="alignright"><?php previous_posts_link('Próximos &raquo;') ?></div>
-		</div>
-	<?php else :
+    <div class="navigation">
+      <div class="alignleft"><?php next_posts_link('&laquo; Anteriores') ?></div>
+      <div class="alignright"><?php previous_posts_link('Próximos &raquo;') ?></div>
+    </div>
+    <?php else :
 
-		if ( is_category() ) { // If this is a category archive
-			printf("<h2 class='center'>Desculpe, mas não há artigos pertencentes à categoria selecionada. </h2>", single_cat_title('',false));
-		} else if ( is_date() ) { // If this is a date archive
-			echo("<h2>Desculpe, mas não há artigos relativos a esta data.</h2>");
-		} else if ( is_author() ) { // If this is a category archive
-			$userdata = get_userdatabylogin(get_query_var('author_name'));
-			printf("<h2 class='center'Desculpe, mas não há artigos pertencentes à %s ainda.</h2>", $userdata->display_name);
-		} else {
-			echo("<h2 class='center'>Nenhum artigo encontrado.</h2>");
-		}
-		get_search_form();
+  if ( is_category() ) { // If this is a category archive
+    printf("<h2 class='center'>Desculpe, mas não há artigos pertencentes à categoria selecionada. </h2>", single_cat_title('',false));
+  } else if ( is_date() ) { // If this is a date archive
+    echo("<h2>Desculpe, mas não há artigos relativos a esta data.</h2>");
+  } else if ( is_author() ) { // If this is a category archive
+    $userdata = get_userdatabylogin(get_query_var('author_name'));
+    printf("<h2 class='center'Desculpe, mas não há artigos pertencentes à %s ainda.</h2>", $userdata->display_name);
+  } else {
+    echo("<h2 class='center'>Nenhum artigo encontrado.</h2>");
+  }
+  get_search_form();
 
-	endif;
-?>
+  endif;
+    ?>
 
-	</div>
+  </div>
+</div>
 
-	<div id="widgets">
-	  <div id="sidebar2">
-	  <ul>
-	  <?php dynamic_sidebar('sidebar2'); ?>
-	</ul>
-	</div>
-	<div id="sidebar">
-	  <ul>
-	  <?php dynamic_sidebar('sidebar1'); ?>
-	</ul>
-	</div>
-        </div>
+<div id="widgets">
+  <div id="sidebar2">
+    <ul>
+      <?php dynamic_sidebar('sidebar2'); ?>
+    </ul>
+  </div>
+  <div id="sidebar">
+    <ul>
+      <?php dynamic_sidebar('sidebar1'); ?>
+    </ul>
+  </div>
+</div>
 
 <?php get_footer(); ?>
