748565c25cd93b21aba557bf398e383f358862eb
Fabrício Soares <eu@fabriciosoares.com>
2010-06-16 17:27:09 -0300

    links do rodape em nova janela #64,cores nos links #63 e titulos das pages #58

--- page.php
+++ page.php
@@ -4,7 +4,8 @@
 
   <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
   <div class="post">
-    <h2><?php the_title(); ?></h2>
+	<span class="title"><?php the_title(); ?></span>
+    <!-- <h2><?php the_title(); ?></h2> -->
     <p class="date"><?php the_time('d/m/Y')?></p>
     <?php the_content('<p class="serif">Leia mais &raquo;</p>'); ?>
     <?php wp_link_pages(array('before'         => '<p><strong>Pages:</strong> ',
