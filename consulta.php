5a5c1c5b017b1dfd35dba9127a0a2f8e795432ef
Thiago Silva <tsilva@sourcecraft.info>
2010-06-18 15:51:29 -0300

    Paginating comments (Closes:#35)

--- consulta.php
+++ consulta.php
@@ -57,6 +57,90 @@ function filterContent(content) {
     return content;
 }
 
+function populateComments(container, comments) {
+  container.html('');
+  /* No comments yet */
+  if (comments.length == 0) {
+    container.html(
+                   '<li class="comment">' +
+                   'Não há propostas para o dispositivo selecionado.' +
+                   '</li>');
+    return;
+  }
+
+    for (var i = 0; i < comments.length; i++) {
+      var obj = comments[i];
+      var infoUser = $('<div>')
+        .addClass('infoUser')
+        .append($('<span>')
+                .addClass('date')
+                .html(formatDate(obj.comment_date)))
+        .append($('<span>')
+                .addClass('user')
+                .html(obj.user_name));
+      var li = $('<li>')
+        .addClass('comment')
+        .append(infoUser)
+        .append('<strong>Opinião</strong>')
+        .append($('<p>').html(formatOpinion(obj.meta.opiniao)));
+
+      if (obj.meta.proposta) {
+        li.append('<strong>Proposta</strong>');
+        li.append($('<p>').html(formatProposal(obj.meta.proposta)));
+      }
+
+      if (obj.meta.contribuicao) {
+        li.append('<strong>Contribuição</strong>');
+        li.append($('<p>').html(filterContent(obj.meta.contribuicao)));
+      }
+
+      if (obj.meta.justificativa) {
+        li.append('<strong>Justificativa</strong>');
+        li.append($('<p>').html(filterContent(obj.meta.justificativa)));
+      }
+
+      if (obj.tags) {
+        var ul = $('<ul>');
+        li.append('<strong>Tags</strong>');
+        for (var t = 0; t < obj.tags.length; t++)
+          ul.append($('<li>').append(obj.tags[t].name));
+        li.append(ul);
+      }
+
+      li.appendTo(container);
+    }
+}
+
+function setup_cpages(ct, total, current, fun) {
+  ct.html("");
+  for (var i = 0; i < total+1; i++) {
+    if (i == current) {
+      ct.append($("<li>").append(i+1))
+        } else {
+      ct.append($("<li>")
+                .append(
+                        $("<a>")
+                        .html(i+1)
+                        .attr("title","Página "+(i+1))
+                        .attr("href", "#")
+                        .attr("name", i)
+                        .click(function() { fun($(this).attr("name")) })))
+    }
+  }
+}
+
+function paginateComments(container, comments) {
+  var ct = $("#comments_pagination");
+  var total_pages =  Math.floor((comments.length+1) / 5);
+  
+  function setCommentsPage(page) {
+    setup_cpages(ct, total_pages, page, setCommentsPage);
+    populateComments(container, comments.slice(page*5, 5 + (page*5)));
+  }
+
+  setup_cpages(ct, total_pages, 0, setCommentsPage);
+}
+
 function loadComments(paragraphId, postId) {
     var query = '{"method":"get_paragraph_comments","params":["' +
         paragraphId + '",' + postId + ']}'
@@ -69,67 +153,28 @@ function loadComments(paragraphId, postId) {
     else
         loadedParagraph = paragraphId;
 
-    /* Clearing up comment list */
+    
+    /* Loading gif */
     container.html(
         '<li class="comment">' +
         '<img src="' + templateUrl + '/images/loading.gif" />' +
         '</li>');
 
     /* Getting comment list */
-    $.getJSON(blogUrl, {dialogue_query:query}, function (comments) {
-        var i;
-        container.html('');
 
-        /* No comments yet */
-        if (comments.length == 0) {
-            container.html(
-                '<li class="comment">' +
-                'Não há propostas para o dispositivo selecionado.' +
-                '</li>');
-            return;
-        }
+    $.getJSON(blogUrl, {dialogue_query:query}, function (comments) {
+        var i;        
 
         /* Building the html of the comment list */
-        for (i = 0; i < comments.length; i++) {
-            var obj = comments[i];
-            var infoUser = $('<div>')
-                .addClass('infoUser')
-                .append($('<span>')
-                        .addClass('date')
-                        .html(formatDate(obj.comment_date)))
-                .append($('<span>')
-                        .addClass('user')
-                        .html(obj.comment_author));
-            var li = $('<li>')
-                .addClass('comment')
-                .append(infoUser)
-                .append('<strong>Opinião</strong>')
-                .append($('<p>').html(formatOpinion(obj.meta.opiniao)));
 
-            if (obj.meta.proposta) {
-                li.append('<strong>Proposta</strong>');
-                li.append($('<p>').html(formatProposal(obj.meta.proposta)));
-            }
-
-            if (obj.meta.contribuicao) {
-                li.append('<strong>Contribuição</strong>');
-                li.append($('<p>').html(filterContent(obj.meta.contribuicao)));
-            }
-
-            if (obj.meta.justificativa) {
-                li.append('<strong>Justificativa</strong>');
-                li.append($('<p>').html(filterContent(obj.meta.justificativa)));
-            }
-
-            if (obj.tags) {
-                var ul = $('<ul>');
-                li.append('<strong>Tags</strong>');
-                for (var t = 0; t < obj.tags.length; t++)
-                    ul.append($('<li>').append(obj.tags[t].name));
-                li.append(ul);
-            }
-
-            li.appendTo(container);
+        //paginate here using $(#comments_pagination)
+        // create a function to populate the comments (the code for this is below)
+        // when page changes, call that func with a subset of the comments.
+        if (comments.length > 5) {
+          populateComments(container, comments.slice(0,5));
+          paginateComments(container, comments);
+        } else {
+          populateComments(container, comments);
         }
     });
 }
@@ -181,19 +226,21 @@ $(document).ready(function () {
   <div id="comments">
     <div id="navegaComments">
       <h2 class="widgettitle">Propostas</h2>
-      <!--
-      <ol>
+      <ol id="comments_pagination">
+<!--
         <li><a href="#" title="Página 1">1</a></li>
         <li><a href="#" title="Página 2">2</a></li>
         <li><a href="#" title="Página 3">3</a></li>
         <li><a href="#" title="Página 4">4</a></li>	
         <li><a href="#" title="Página 5">5</a></li>
+-->
       </ol>
+<!--
       <form action="get">
         <label for="">Campo busca de comentários</label>
         <input type="text" name="busca nos comentários" />
       </form>
-      -->
+-->
     </div><!--fim #navegaComments-->
     <ul id="commentContainer"></ul>
   </div><!--fim #comments-->
