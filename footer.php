a3dc20f573e92ef3374bc0469b04c5654f7feeac
Fabricio Soares <eu@fabriciosoares.com>
2010-06-29 18:17:31 -0300

    Tickets #25, #69 and #76

--- footer.php
+++ footer.php
@@ -1,12 +1,12 @@
-      div id="footer">
+     <div id="footer">
         <ul>
 	  <li><a href="http://www.cultura.gov.br/consultadireitoautoral/termos-de-uso/" title="Termos de uso">Termos de uso</a></li>
 	  <li><a href="http://www.cultura.gov.br/consultadireitoautoral/sobre/" title="Sobre a Revisão da Lei de Direito Autoral">Sobre</a></li>
 	  <li><a href="http://www.cultura.gov.br/consultadireitoautoral/processo-colaborativo-2/"
 	  title="Sobre o processo de desenvolvimento da consulta colaborativa">Processo Colaborativo</a></li>
 	  <li><a href="http://culturadigital.br/" title="Acesso ao Cultura Digital">Cultura Digital</a></li>
-	  <li><a href="http://www.wordpress.org/" title="Plataforma de publicação de conteúdo e blogs">Wordpress</a></li>
-	  <li><a href="http://xemele.cultura.gov.br/git" title="Repositório de código do Cultura Digital">Código Fonte</a></li>
+	  <li><a href="http://www.wordpress.org/" title="Plataforma de publicação de conteúdo e blogs" target="_blank">Wordpress</a></li>
+	  <li><a href="http://xemele.cultura.gov.br/git" title="Repositório de código do Cultura Digital" target="_blank">Código Fonte</a></li>
 	  <li><a href="http://www.cultura.gov.br/consultadireitoautoral/wp-login.php?action=register" title="Cadastre-se">Cadastre-se</a></li>
 	</ul>
         <?php wp_footer(); ?>
